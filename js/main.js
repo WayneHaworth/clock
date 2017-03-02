b631df4163f5d71006b4065dd1a2c773d9d34f70
Wayne Haworth <waynehaworth@MacBook-Pro.local>
2017-03-02 09:08:59 +0000

    added ticking sound and mute button

--- js/main.js
+++ js/main.js
@@ -24,7 +24,27 @@ $(document).ready(function() {
       $('.second').css("transform", "rotate(" + (secondsToDegree() + 360) + "deg)");
       $('.minute').css("transform", "rotate(" + (minutesToDegree() + 360) + "deg)");
       $('.hour').css("transform", "rotate(" + (hoursToDegree() + 360) + "deg)");
-      tick[0].play();
+      
+
+      if ($('.volume').hasClass('fa-volume-up')) {tick[0].play();}
   }, 1000);
+
+  $('.volume').on('click', function() {
+    if ($(this).hasClass('fa-volume-up')) {
+      $(this).addClass('fa-volume-off');
+      $(this).removeClass('fa-volume-up');
+      window.console.log("un-mute");
+      tick[0].play();
+      return 1;
+    }
+    if ($(this).hasClass('fa-volume-off')) {
+      $(this).addClass('fa-volume-up');
+      $(this).removeClass('fa-volume-off');
+      window.console.log("mute");
+      tick[0].pause();
+      return 1;
+    }
+    else { window.console.log("Error");}
+  });
 });
 
